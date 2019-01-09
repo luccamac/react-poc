aeb391043b8c11e12bd2195181cf28096226fbf7
Lucca <lucca_santos@dellteam.com>
2019-01-09 10:50:00 -0200

    Some refactoring, changed the Array to List and started to implement the Delete method.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+
   constructor(props) {
     super(props);
     this.state = {
-      value: 'Volkswagen Gol'
+      value: '',
+      successMessage: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -15,27 +20,32 @@ class App extends Component {
 
   handleChange(event) {
     this.setState({value: event.target.value});
+    this.setState({ successMessage: ''});
   }
 
   handleSubmit(event) {
-    // var headers = { 'Content-Type': 'application/json' };
     var car = { "name": this.state.value };
     event.preventDefault();
-    axios.post('http://localhost:8001/cars', car)
-    // , { headers: headers }
-    .then(response => console.log("Submitted with success!"),
-          error => "Error submitting the form.");
+    axios.post('http://localhost:3000/cars', car)
+    .then(response => { 
+      console.log(JSON.stringify(response)); this.setState({ successMessage: 'Success!'})}, 
+      error => { console.log(error);  this.setState({ successMessage: 'Failed to Submit :('})});
+  }
+
+  clearForm(){
+    this.setState({ value: ''});
+    this.render();
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <p>Garage</p>
-        <label>Your Car Name:
-        <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-        <a href="/cars-list">Go to Cars List</a>
+      <h1>Garage</h1>
+      <p id="carLabel">Your Car Name:</p>
+      <p>{this.state.successMessage}</p>
+      <Input type="text" id="nameInput" defaultValue={this.state.value} onChange={this.handleChange} />
+      <input type="submit" id="submit" value="Submit" />
+      <Button variant="contained" id="carsListButton" href="/cars-list">Go to Cars List!</Button>
       </form>
     );
   }
--- src/CarsList.js
+++ src/CarsList.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import JsonTable from 'ts-react-json-table';
+import Button from '@material-ui/core/Button';
 
 class CarsList extends Component {
-
-    // carsList = [];
 
     constructor(props) {
         super(props);
         this.state = {
-            carsList: []
+            carsList: [],
+            successMessage: ''
         }
         this.callService();
         this.render();
     }
 
     callService() {
-        return axios.get('http://localhost:8001/cars')
+        axios.get('http://localhost:3000/cars')
         .then(res => {
             const cars = res.data;
             console.log(cars);
             this.setState({ carsList: cars});
             });
         }
- 
-        render() {
-            return (<JsonTable className="table table-striped" rows={this.state.carsList} />); }
+
+    onClickRow(e, item){
+        console.log('e' + e);
+        console.log('item' + item);
     }
+ 
+    render() {
+        return (
+        <div>
+            <JsonTable rows={this.state.carsList} columns={['Id', 'Name']} onClickRow={this.onClickRow} />
+            <Button variant="contained" id="carsListButton" href="/">Go to Cars Form!</Button>
+        </div>
+        );
+    }
+}
 
 
 export default CarsList;
